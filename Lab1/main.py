9f9848328d04d680fdd896e1044af5c8bb595b1e
AnnaYedka <anna.yedka@gmail.com>
2023-02-28 16:36:25 +0300

    added 2nd task

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,2 +1,22 @@
-#1
+# 1
 print("Hello world")
+
+# 2
+ADD = "add"
+SUB = "sub"
+MULT = "mult"
+DIV = "div"
+
+
+def math_operations(num1, num2, op):
+    if op == ADD:
+        return num1 + num2
+    elif op == SUB:
+        return num1 - num2
+    elif op == MULT:
+        return num1 * num2
+    elif op == DIV:
+        return num1 / num2
+    else:
+        print("invalid operation")
+        return 0
