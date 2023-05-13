99423aa7dc2da0b2d280f36c9c657e4d3bb9254e
AnnaYedka <anna.yedka@gmail.com>
2023-05-13 19:08:49 +0300

    added convertion from str to dict of primitives

--- lab3/JSONSerializer.py
+++ lab3/JSONSerializer.py
@@ -1,3 +1,5 @@
+import re
+
 from serializer import Serializer
 from consts import JSON_STYLE_VALUES
 
@@ -7,14 +9,13 @@ class JSONSerializer(Serializer):
         pass
 
     def dumps(self, obj) -> str:
-        pass
+        return self._serialize(obj)
 
     def load(self, fp):
         pass
 
-    def loads(self, s):
+    def loads(self, s: str):
         pass
-
 
     def _serialize(self, obj, indent=0) -> str:
         if type(obj) == dict:
@@ -22,8 +23,10 @@ class JSONSerializer(Serializer):
                 return "{}"
             tmp = "{"
             for key in list(obj)[:-1]:
-                tmp += ",\n" + "    " * indent + self._serialize(key) + ": " + self._serialize(obj[key], indent + 1)
-            return tmp + self._serialize(list(obj)[-1]) + ": " + self._serialize(obj[list(obj)[-1]], indent + 1) + "\n" + "    "*indent + "}"
+                tmp += "\n" + "    " * indent + self._serialize(key) + ": " + self._serialize(obj[key],
+                                                                                              indent + 1) + ","
+            return tmp + "\n" + "    " * indent + self._serialize(list(obj)[-1]) + ": " + \
+                   self._serialize(obj[list(obj)[-1]], indent + 1) + "\n" + "    " * indent + "}"
 
         if type(obj) == str:
             return f'"{obj}"'
@@ -39,3 +42,77 @@ class JSONSerializer(Serializer):
 
         if obj in JSON_STYLE_VALUES.keys():
             return JSON_STYLE_VALUES[obj]
+
+    def _deserialize(self, s: str) -> dict:
+        lines = s.split("\n")
+        return self._deserialize_line(lines)
+
+    def _deserialize_line(self, lines):
+        it = 0
+        if lines[it] == "{":
+            it = 1
+        tmp = {}
+
+        for j in range(it, len(lines)):
+            line = lines[j]
+
+            if j < it:
+                continue
+
+            if re.search(r"(.+): (.+),?", line):
+                match = re.search(r"(.+): (.+),?", line)
+                key = match.group(1)
+                val = match.group(2)
+                j += 1
+                if val == "{":
+                    open_count = 0
+                    close_count = 0
+                    for i in range(j, len(lines)):
+                        open_count += lines[i].count("{")
+                        close_count += lines[i].count("}")
+                        if close_count > open_count:
+                            val = self._deserialize_line(lines[it+1:i+1])
+                            it = i+1
+                            break
+                    tmp.update({self._deserialize_line([key]): val})
+                else:
+                    tmp.update({self._deserialize_line([key]): self._deserialize_line([val])})
+
+            elif "}" in line:
+                return tmp
+
+            elif "[" in line:
+                open_count = 0
+                close_count = 0
+                it += 1
+                for i in range(it, len(lines)):
+                    open_count += lines[i].count("[")
+                    close_count += lines[i].count("]")
+                    if close_count > open_count:
+                        it = i
+                        return self._deserialize_list(lines[it - 1:i])
+            else:
+                return self._deserialize_primitive(line)
+
+    def _deserialize_list(self, s: str):
+        values = s[1:-1].split(", ")
+        l = []
+        for val in values:
+            l.append(self._deserialize_line([val]))
+        return tuple(l)
+
+    def _deserialize_primitive(self, s: str):
+        if s == "null":
+            return None
+        if s == "true":
+            return True
+        if s == "false":
+            return False
+        if re.search(r"\"(.+)\"", s):
+            return re.search(r"\"(.+)\"", s).group(1)
+        if re.search(r"-?[0-9]+\.[0-9]+", s):
+            return float(re.search(r"-?[0-9]+\.[0-9]+", s).group())
+        if re.search(r"\(-?[0-9]+(\+|-)[0-9]+j\)", s):
+            return complex(re.search(r"\(-?[0-9]+(\+|-)[0-9]+j\)", s).group())
+        if re.search(r"-?[0-9]+", s):
+            return int(re.search(r"[0-9]+", s).group())
--- lab3/consts.py
+++ lab3/consts.py
@@ -1,3 +1,4 @@
 PRIMITIVE_TYPES = (int, float, complex, str, bool, type(None))
 ITERABLE_TYPES = (list, set, tuple)
 NOT_SERIALIZABLE = ('__weakref__', '__dict__')
+JSON_STYLE_VALUES = {True: "true", False: "false", None: "null"}
