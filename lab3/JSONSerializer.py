294ba9b40868883b83a5f71c97900b0cc42b8dbf
AnnaYedka <anna.yedka@gmail.com>
2023-05-16 23:35:39 +0300

    fixed&tested json deser.

--- lab3/JSONSerializer.py
+++ lab3/JSONSerializer.py
@@ -18,6 +18,8 @@ class JSONSerializer(Serializer):
             return self.loads(file.read())
 
     def loads(self, s: str):
+        s = s.replace("\n", "")
+        s = s.replace("    ", "")
         return deconvert(self._deserialize(s))
 
     def _serialize(self, obj, indent=0) -> str:
@@ -46,64 +48,116 @@ class JSONSerializer(Serializer):
         if obj in JSON_STYLE_VALUES.keys():
             return JSON_STYLE_VALUES[obj]
 
-    def _deserialize(self, s: str) -> dict:
-        lines = s.split("\n")
-        return self._deserialize_line(lines)
-
-    def _deserialize_line(self, lines):
-        j = 0
-        if lines[j] == "{":
-            j = 1
-        tmp = {}
-
-        while j < len(lines):
-            line = lines[j]
-
-            if re.search(r"\s*(.+): (.+)", line):
-                match = re.search(r"\s*(.+): (.+)", line)
-                key = match.group(1)
-                val = match.group(2)
-                j += 1
-                if val == "{":
-                    open_count = 0
-                    close_count = 0
-                    for i in range(j, len(lines)):
-                        open_count += lines[i].count("{")
-                        close_count += lines[i].count("}")
-                        if close_count > open_count:
-                            val = self._deserialize_line(lines[j:i+1])
-                            j = i+1
-                            break
-                    tmp.update({self._deserialize_line([key]): val})
-                else:
-                    tmp.update({self._deserialize_line([key]): self._deserialize_line([val])})
-
-            elif "}" in line:
-                return tmp
-
-            elif "[" in line:
-                j += 1
-                open_count = 0
-                close_count = 0
-                for i in range(j, len(lines)):
-                    open_count += lines[i].count("[")
-                    close_count += lines[i].count("]")
-                    if close_count > open_count:
-                        return self._deserialize_list("".join(lines[j:i+1]))
-            else:
-                return self._deserialize_primitive(line)
-
-    def _deserialize_list(self, s: str):
-        if s[-1] == ",":
-            values = s[1:-2].split(", ")
+    def _deserialize(self, s: str):
+        i = 0
+        if s[i] == " ":
+            i+=1
+        if s[i] == "{":
+            return self._deserialize_dict(s, i)[0]
+        elif s[i] == "[":
+            return self._deserialize_list(s, i)[0]
         else:
-            values = s[1:-1].split(", ")
+            return self._deserialize_primitive(s)
+    def _deserialize_dict(self, s, i):
+        tmp = {}
+        i += 1
+        open_count = 0
+        close_count = 0
+        key = ""
+        val = ""
+        update_key = True
+        check_end = True
+        while True:
+            if check_end and s[i] == ":":
+                update_key = False
+                i += 1
+            elif check_end and s[i] == ",":
+                update_key = True
+                if type(key) != str:
+                    tmp.update({key: self._deserialize(val)})
+                elif type(val) != str:
+                    tmp.update({self._deserialize(key): val})
+                else:
+                    tmp.update({self._deserialize(key): self._deserialize(val)})
+                key = ""
+                val = ""
+                i += 1
+            if s[i] == "{":
+                check_end = False
+                open_count += 1
+            elif s[i] == "}":
+                close_count += 1
 
-        l = []
-        for val in values:
-            if val:
-                l.append(self._deserialize_line([val]))
-        return tuple(l)
+            elif s[i] == "[" and check_end:
+                if update_key:
+                    key, i = self._deserialize_list(s, i)
+                else:
+                    val, i = self._deserialize_list(s, i)
+                continue
+
+            if close_count and close_count == open_count:
+                check_end = True
+            if close_count > open_count:
+                if type(key) != str:
+                    tmp.update({key: self._deserialize(val)})
+                elif type(val) != str:
+                    tmp.update({self._deserialize(key): val})
+                else:
+                    if key == "":
+                        i += 1
+                        return tmp, i
+                    tmp.update({self._deserialize(key): self._deserialize(val)})
+                i+=1
+                break
+
+            if update_key:
+                key += s[i]
+            else:
+                val += s[i]
+            i += 1
+        return tmp, i
+
+    def _deserialize_list(self, s, i):
+        tmp = []
+        val = ""
+        open_count = 0
+        close_count = 0
+        i += 1
+        check_end = True
+        while True:
+            if check_end and s[i] == ",":
+                if type(val) != str:
+                    tmp.append(val)
+                else:
+                    tmp.append(self._deserialize(val))
+                val = ""
+                i += 1
+
+            if s[i] == "[":
+                check_end = False
+                open_count += 1
+            elif s[i] == "]":
+                close_count += 1
+            elif s[i] == "{" and check_end:
+                val, i = self._deserialize_dict(s, i)
+                continue
+
+            if close_count > open_count:
+                if type(val) != str:
+                    tmp.append(val)
+                elif val == "":
+                    i+=1
+                    return tuple(tmp), i
+                else:
+                    tmp.append(self._deserialize(val))
+                i+=1
+                break
+
+            if close_count and close_count == open_count:
+                check_end = True
+            val += s[i]
+            i += 1
+        return tuple(tmp), i
 
     def _deserialize_primitive(self, s: str):
         if s == "null":
@@ -112,8 +166,8 @@ class JSONSerializer(Serializer):
             return True
         if s == "false":
             return False
-        if re.search(r"\"(.+)\"", s):
-            return re.search(r"\"(.+)\"", s).group(1)
+        if re.search(r"\"(.*)\"", s):
+            return re.search(r"\"(.*)\"", s).group(1)
         if re.search(r"-?[0-9]+\.[0-9]+", s):
             return float(re.search(r"-?[0-9]+\.[0-9]+", s).group())
         if re.search(r"\(-?[0-9]+(\+|-)[0-9]+j\)", s):
