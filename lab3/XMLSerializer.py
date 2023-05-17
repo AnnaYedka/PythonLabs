e8fbb0d56adcc1f47547d21ffefc8fe8774620fa
AnnaYedka <anna.yedka@gmail.com>
2023-05-17 23:07:00 +0300

    raw version of deser.

--- lab3/XMLSerializer.py
+++ lab3/XMLSerializer.py
@@ -1,7 +1,7 @@
 from serializer import Serializer
 from consts import PRIMITIVE_TYPES, ITERABLE_TYPES
 from Converter import convert, deconvert
-
+import re
 class XMLSerializer(Serializer):
     def dump(self, obj, fp):
         with open(fp, "w") as file:
@@ -32,13 +32,75 @@ class XMLSerializer(Serializer):
             tmp += "\n"+"    "*indent + "</list>" + "\n"
             return tmp
 
-        tmp = ""
+        tmp = "\n"
         for k, v in obj.items():
             if type(v) in PRIMITIVE_TYPES:
-                tmp += "\n" + "    " * indent + f"<{self._serialize(k, indent + 1)}>{self._serialize(v, indent + 1)}" +\
-                        f"</{self._serialize(k, indent + 1)}>"
+                tmp += "    " * indent + f"<{self._serialize(k, indent + 1)}>{self._serialize(v, indent + 1)}" +\
+                        f"</{self._serialize(k, indent + 1)}>" + "\n"
             else:
-                tmp += "\n"+"    "*indent + f"<{self._serialize(k, indent+1)}>{self._serialize(v, indent+1)}" +\
-                   "    "*indent + f"</{self._serialize(k, indent+1)}>"
-        return tmp + "\n"
+                tmp += "    "*indent + f"<{self._serialize(k, indent+1)}>{self._serialize(v, indent+1)}" +\
+                   "    "*indent + f"</{self._serialize(k, indent+1)}>" + "\n"
+        return tmp
 
+
+    def _deserialize(self, s:str):
+        keys = []
+        vals = []
+        i = 0
+        tmp = ""
+        has_value = False
+        while i < len(s):
+            if s[i] == "<":
+                if has_value:
+                    vals.append(self._deserialize_primitive(tmp))
+                tmp += s[i]
+            elif s[i] == ">":
+                tmp += s[i]
+                keys.append(tmp)
+                if tmp == "<list>":
+                    vals.append([])
+                else:
+                    vals.append({})
+                tmp = ""
+                if i != len(s) and s[i+1] != "<":
+                    has_value = True
+            elif s[i] == "/":
+                tmp = ""
+                while s[i] != ">":
+                    i += 1
+                if keys[-1] == "<item>":
+                    vals[-2].append(self._deserialize_primitive(vals[-1]))
+                    vals.pop()
+                else:
+                    vals[-2].update({self._deserialize_primitive(keys[-1][1:-1]): self._deserialize_primitive(vals[-1])})
+                    vals.pop()
+                    keys.pop()
+                has_value = False
+            else:
+                tmp += s[i]
+            i+=1
+        res = {}
+        if len(s) < 6 or (len(s) > 5 and s[:6] != "<list>"):
+            for d in vals:
+                res.update(d)
+        else:
+            res = vals[0]
+        return res
+
+
+    def _deserialize_primitive(self, s: str):
+        if type(s) != str:
+            return s
+        if s == "None":
+            return None
+        if s == "True":
+            return True
+        if s == "False":
+            return False
+        if re.search(r"-?[0-9]+\.[0-9]+", s):
+            return float(re.search(r"-?[0-9]+\.[0-9]+", s).group())
+        if re.search(r"\(-?[0-9]+(\+|-)[0-9]+j\)", s):
+            return complex(re.search(r"\(-?[0-9]+(\+|-)[0-9]+j\)", s).group())
+        if re.search(r"-?[0-9]+", s):
+            return int(re.search(r"[0-9]+", s).group())
+        return s
